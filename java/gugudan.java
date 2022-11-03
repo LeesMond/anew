bb80b9747b708ad38e81be0e87ea00c6b0c2f94e
LeesMond <ljm71555555@gmail.com>
2022-11-03 15:03:34 +0900

    day2

--- java/gugudan.java
+++ java/gugudan.java
@@ -1,21 +1,23 @@
-import java.util.Scanner;
+import java.util.*;
 
-public class gugudan{
+public class gugudan {
+	public static void main(String[] agrs) {
 
-  public static void main(String[] args){
-      Scanner sc = new Scanner(System.in);
-      while(true){
-        System.out.print("Insert num here >> ");
-        int a = sc.nextInt();
-        if(a == 0){
-          break;
-        }else if( a > 1 && a < 10){
-          for(int i = 1; i <= 9; i++){
-            System.out.println(a + " * " + i + " = " + (a*i));
-          }
-        }else{
-          System.out.print("retry   ");
+    while(true){
+      Scanner scan = new Scanner(System.in);
+  		System.out.print("Insert number(0:Exit ) : ");
+  		int num = scan.nextInt();
+
+      if(num==0){
+        break;
+      } else if(num>0 && num<10) {
+        for(int i=1; i<=9; i++) {
+          System.out.println(num+"*"+i+"="+num*i);
         }
+      } else {
+        System.out.println("Insert number 2~9. Retype Again ");
       }
     }
+
+	}
 }
